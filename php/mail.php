da1dadc812fcbfbbf21c81e253c5b8c672954728
Nicolás Pennesi <nicolaspennesi@gmail.com>
2018-02-06 01:40:49 -0300

    Fix mail.php

--- php/mail.php
+++ php/mail.php
@@ -1,6 +1,6 @@
 <?php
 
-$to = "nicolaspennesi@gmail.com";
+$to = "hi@nicolaspennesi.com";
 
 // Setea variables
 $name = $_POST["callName"];
@@ -14,12 +14,12 @@ $message .= "<br><br>Email: " . $email;
 $message .= "<br><br>Mensaje: " . $mensaje;
 $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
-$headers .= "From: Formulario Web <web@skirience.com>" . "\r\n";
+$headers .= "From: Web CV <web@nicolaspennesi.com>" . "\r\n";
 $headers .= "Reply-To: " . $name . " <" . $email . ">" . "\r\n";
 
 //Envía Mail
 if (mail($to, $subject, $message, $headers)) {
     echo '{"response":"success"}';
 } else {
-    echo "Error al enviar mensaje, intente nuevamente más tarde.";
+    echo "Error sending message, please try again later.";
 }
